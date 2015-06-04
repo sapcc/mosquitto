8c8836780025842b9ced9dd918a0bf53f298417d
Fabian Ruff <fabian.ruff@sap.com>
2015-06-04 17:32:55 +0200

    Add libldap to auth plugin for dn parsing
    This pulls in a dependency to libldap and libsasl which adds about 2,5MB to the resulting image :(
    
    But on the plus side I get a fully working parser for rfc 4514 formated distinguished names.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV http_proxy=http://proxy.***REMOVED***:8080 \
 ADD build.key.pub /etc/apk/keys/
 ADD pkgs /pkgs
 RUN echo "@local /pkgs" >> /etc/apk/repositories
-RUN apk --update add mosquitto@local mosquitto-clients@local mosquitto-auth-monsoon@local
+RUN apk --update add mosquitto@local mosquitto-clients@local mosquitto-auth-monsoon@local && rm -rf /var/cache/apk/*
 RUN mkdir -p /etc/mosquitto/conf.d
 ADD mosquitto.conf /etc/mosquitto/mosquitto.conf
 EXPOSE 1883
--- mosquitto-auth-monsoon/mosquitto_auth_plugin_monsoon.c
+++ mosquitto-auth-monsoon/mosquitto_auth_plugin_monsoon.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mosquitto.h>
 #include <mosquitto_plugin.h>
+#include <ldap.h>
 
 int mosquitto_auth_plugin_version(void) {
   return MOSQ_AUTH_PLUGIN_VERSION;
@@ -28,6 +30,44 @@ int mosquitto_auth_security_cleanup(void *user_data, struct mosquitto_auth_opt *
 
 int mosquitto_auth_acl_check(void *user_data, const char *clientid, const char *username, const char *topic, int access) {
   printf("FABUS: mosquitto_auth_acl_check(clientid: %s, username: %s, topic: %s, access: %d)\n", clientid, username, topic, access);
+  LDAPDN dn = NULL;
+  int err = ldap_str2dn(username, &dn , LDAP_DN_FORMAT_LDAPV3 | LDAP_DN_PEDANTIC);
+  if (err != 0) {
+    printf("FABUS: error parsing dn: %d)\n", err);
+    ldap_dnfree(dn);
+    return MOSQ_ERR_ACL_DENIED;
+  }
+  LDAPAVA *attr = NULL;
+  LDAPRDN rdn = NULL;
+  char* common_name = NULL;
+  char* organizational_unit = NULL; 
+  char* organization = NULL;
+  /* iterate over DN components: e.g. cn=a+sn=b */
+  int idx;
+	for (idx = 0; dn[idx] != NULL; idx++) {
+    rdn = dn[idx];
+    attr = rdn[0];
+    if ((attr->la_flags & LDAP_AVA_STRING) == 0) {
+      //skip non string attributes
+      continue;
+    }
+    if (attr->la_attr.bv_len == 2 && strncasecmp("CN", attr->la_attr.bv_val, 2) == 0) {
+          common_name = strdup(attr->la_value.bv_val);
+          printf("Common Name: %s\n", common_name);
+    }
+    if (attr->la_attr.bv_len == 2 && strncasecmp("OU", attr->la_attr.bv_val, 2) == 0) {
+          organizational_unit = strdup(attr->la_value.bv_val);
+          printf("Organizational Unit: %s\n", organizational_unit);
+    }
+    if (attr->la_attr.bv_len == 1 && strncasecmp("O", attr->la_attr.bv_val, 1) == 0) {
+          organization = strdup(attr->la_value.bv_val);
+          printf("Organization: %s\n", organization);
+    }
+  }
+  if (common_name) free(common_name);
+  if (organizational_unit) free(organizational_unit);
+  if (organization) free(organization);
+  ldap_dnfree(dn);
   return MOSQ_ERR_SUCCESS;
 }
 
